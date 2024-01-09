eff3569c1d99ce5776b67dc4a7a5965070253972
Rruuu <FazilovR@yandex.ru>
2024-01-09 23:38:35 +0300

    фикс по ревью

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,8 +11,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-# ALLOWED_HOSTS = ['158.160.28.14', '127.0.0.1', 'localhost', 'kittygrm.sytes.net']
-
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
